0ded23af8ff548f86f4786219acfca9f26c7f02a
Piotr Demichowicz <piotr.demichowicz@gmail.com>
2018-11-05 18:29:52 +1300

    merged the right way

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-<<<<<<< HEAD
-            Edit <code>src/App.js</code> and save to reload.
-=======
             You feel veeeery sleepy...
           </p>
 		  <p>
             Eat your left arm. NOW!!!
->>>>>>> 54adbb5629d33627b5611d3283bd09fe5b4e054c
           </p>
           <a
             className="App-link"
@@ -24,11 +20,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-<<<<<<< HEAD
-            Learn React
-=======
             Lick my elbow
->>>>>>> 54adbb5629d33627b5611d3283bd09fe5b4e054c
           </a>
         </header>
       </div>
