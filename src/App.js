70863d500b22a83faf52b7b16624d40cfee8bc56
Piotr Demichowicz <piotr.demichowicz@gmail.com>
2018-11-05 19:14:34 +1300

    test change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             You feel veeeery sleepy...
+            And now you sleep!!!!
           </p>
 		  <p>
             Eat your left arm. NOW!!!
