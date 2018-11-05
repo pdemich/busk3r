27d5b29be53c52dd4a8ef7e1ff172ff4603230f8
Piotr Demichowicz <piotr.demichowicz@gmail.com>
2018-11-05 22:14:30 +1300

    Add table to app as a separate component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Table from './Table';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hello, React!</h1>
+            <div className="container">
+               <Table />
             </div>
         );
     }
