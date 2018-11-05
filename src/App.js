70347948a4510a5ad4998d19103f6f88e6b6f532
Piotr Demichowicz <piotr.demichowicz@gmail.com>
2018-11-05 23:24:41 +1300

    add removal of table rows with more magic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
 import Table from './Table';
 class App extends Component {
+    state = {
+        characters: [
+            {
+            'name': 'Charlie',
+            'job': 'Janitor'
+        },
+        {
+            'name': 'Mac',
+            'job': 'Bouncer'
+        },
+        {
+            'name': 'Dee',
+            'job': 'Aspring actress'
+        },
+        {
+            'name': 'Dennis',
+            'job': 'Bartender'
+        }
+        ]
+    };
     render() {
-        const characters = [
-            {
-                'name': 'Charlie',
-                'job': 'Janitor'
-            },
-            {
-                'name': 'Mac',
-                'job': 'Bouncer'
-            },
-            {
-                'name': 'Dee',
-                'job': 'Aspring actress'
-            },
-            {
-                'name': 'Dennis',
-                'job': 'Bartender'
-            }
-        ];
+        
         return (
             <div className="container">
-               <Table characterData={characters}/>
+               <Table characterData={this.state.characters}
+                      removeCharacter={this.removeCharacter}
+               />
             </div>
         );
+    }
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
     }
 }
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -5,6 +5,7 @@ const TableHeader = () => {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th></th>
             </tr>
         </thead>
     );
@@ -16,6 +17,7 @@ const TableBody = props => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td>
             </tr>
         );
     });
@@ -25,11 +27,13 @@ const TableBody = props => {
 
 class Table extends Component {
     render() {
-        const { characterData } = this.props;
+        const { characterData, removeCharacter } = this.props;
         return (
             <table>
                 <TableHeader/>
-                <TableBody characterData={characterData}/>
+                <TableBody characterData={characterData}
+                           removeCharacter={removeCharacter} 
+                />
             </table>
         );
     }
