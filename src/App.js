6842477ba4b7da90523819e30370c42db720edc6
Piotr Demichowicz <piotr.demichowicz@gmail.com>
2018-11-05 21:29:07 +1300

    Clean react app

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            You feel veeeery sleepy...
-            And now you sleep!!!!
-          </p>
-		  <p>
-            Eat your left arm. NOW!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Lick my elbow
-          </a>
-        </header>
-      </div>
+      <h1>Busk3r initial look</h1>
     );
   }
 }
