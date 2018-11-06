6c939ffe290b15a2ae3417a2d71b1a71c0a535d9
Piotr Demichowicz <piotr.demichowicz@gmail.com>
2018-11-06 21:53:54 +1300

    formatting fix

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import './Menu.css';
 class App extends Component {
   render() {
     return (
-	<div >
-      <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-		<a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/">About</a>
-        <a id="contact" className="menu-item" href="/">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-		</Menu>
-		<main id="page-wrap">
-		
-		</main>
-	</div>	
+      <div id="outer-container">
+        <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/">About</a>
+          <a id="contact" className="menu-item" href="/">Contact</a>
+          <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
+        </Menu>
+        <main id="page-wrap">
+
+        </main>
+      </div>
     );
   }
 }
