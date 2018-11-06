49017a92d0935bbdf55f475a13366c98302273fd
Peter Demich <Peter.D@clubware.co.nz>
2018-11-06 15:51:46 +1300

    add menu to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { slide as Menu } from 'react-burger-menu'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Menu.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>Busk3r initial look</h1>
+	<div id="outer-container">
+      <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
+		<a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/">About</a>
+        <a id="contact" className="menu-item" href="/">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+		</Menu>
+		<main id="page-wrap">
+		
+		</main>
+	</div>	
     );
   }
 }
